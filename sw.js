9ab6c77dad741c6babb3b13ae38a261b41efbca1
Samiran95 <132401863+Samiran95@users.noreply.github.com>
2023-09-09 07:24:56 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,28 @@
-//console.log('service worker inside sw.js');
+const cacheName = "app-shell-rsrs";
+const assets = [
+    '/',
+    'index.html',
+    'JS/animate.js',
+    'JS/app.js',
+    'JS/count.js',
+    'JS/grocfunction.js',
+    'JS/gsap.min.js',
+    'JS/othfunction.js',
+    'CSS/dashboard.css',
+    'fontawesome/css/all.css',
+    'fontawesome/sprites/solid.svg',
+    'https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap'
+];
+
 
 //install service worker
 self.addEventListener('install', evt => {
-    console.log('service worker have been installed');
+    evt.waitUntil(
+        caches.open(cacheName).then(cache => {
+            cache.addAll(assets);
+        })
+    );
+
 });
 
 //activate
@@ -10,3 +30,14 @@ self.addEventListener('activate', evt =>{
     console.log('service worker have been activated')
 })
 
+//fetch event
+self.addEventListener('fetch', evt =>{
+    console.log(evt);
+
+    evt.respondWith(
+        caches.match(evt.request).then(cacheRes =>{
+            return cacheRes || evt.request;
+        })
+    );
+
+})
